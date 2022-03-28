b7a3fbc55dbdf1eeb0719c17034968971cd91278
HEKUCHAN <Heitorhirose@gmail.com>
2022-03-28 22:49:30 +0900

    choi henkou

--- minecraft_backup/__main__.py
+++ minecraft_backup/__main__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
-from . import main
-
-main.main()
+if __name__ == '__main__':
+    from . import main
+    try:
+        main.main()
+    except KeyboardInterrupt:
+        pass
