dd2140bc2436d74894b01351aec18effbc92330d
HEKUCHAN <Heitorhirose@gmail.com>
2022-03-28 23:15:39 +0900

    setup.pyを調整

--- setup.py
+++ setup.py
@@ -10,7 +10,11 @@ setup(
     version="1.0.0",
     packages=find_packages(),
     python_requires=">=3.6.8",
-    entry_points={"console_scripts": {"minecraft-backup=minecraft_backup.main:main"}},
+    entry_points={
+        "console_scripts": {
+            "minecraft-backup=minecraft_backup.main:main"
+        }
+    },
     description="""\
         This is a backup management library for Minecraft servers.
         It should be run and used on a regular basis using cron or similar.
@@ -27,9 +31,15 @@ setup(
         "Programming Language :: Python :: 3.6",
     ],
     include_package_data=True,
-    keywords=["Image Registration"],
+    keywords=[
+        "Image Registration"
+    ],
     license="MIT License",
-    install_requires=["pathlib", "argparse", "fabric3"],
+    install_requires=[
+        "pathlib",
+        "argparse",
+        "fabric3"
+    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
 )
